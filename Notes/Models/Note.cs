ca4b476bcaec84709e5fe15ed178115dbd4e7df7
DominicB-yte <53456554+DominicB-yte@users.noreply.github.com>
2021-04-27 19:25:13 +1200

    Initial commit

---
+++ Notes/Models/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SQLite;
+
+namespace Notes.Models
+{
+    public class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Food { get; set; }
+        public string Chef { get; set; }
+        public string Ingredients { get; set; }
+        public string Steps { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
